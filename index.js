84ad5a523a025aa954831dec764a8ea441f75a40
AdamGantengg <142318468+AdamGantengg@users.noreply.github.com>
2023-08-15 22:17:12 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.querySelector('a[href^="https://github.com/AdamGantengg/Galeri/settings/pages"]').addEventListener('click', function(e) {
+document.querySelector('a[href^="https://adamgantengg.github.io/Galeri/"]').addEventListener('click', function(e) {
     e.preventDefault();
     document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth'
